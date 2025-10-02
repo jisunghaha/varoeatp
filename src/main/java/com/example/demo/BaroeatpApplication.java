d3e70908b1ae25a3c403683f61de5b212fd94185
DESKTOP-V0K3QAK\tlsgp <wltjd5306@naver.com>
2025-10-02 17:07:53 +0900

    feat: Initial commit with .gitignore

---
+++ src/main/java/com/example/demo/BaroeatpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroeatpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaroeatpApplication.class, args);
+	}
+
+}
